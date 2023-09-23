27337447b4e1f62b6911ff83ab9cb5edd4bbb4af
MZhurova <mzhurova4@mail.ru>
2023-09-23 21:06:32 +0700

    Добавили строку через PyCharm

---
+++ test_lesson2.py
@@ -0,0 +1,23 @@
+import pytest
+from selene.support.shared import browser
+from selene import be, have
+
+
+@pytest.fixture
+def open():
+     pass
+#     browser.open('https://google.com')
+
+
+
+def test_search():
+     pass
+#     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+ #    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+def cloce():
+     pass
+
+
+
+
