2c5f5af36dc06b2cf9c5951c7abf407dfcfe6a61
MZhurova <mzhurova4@mail.ru>
2023-09-23 21:15:35 +0700

    Добавили библиотеки selene

--- test_lesson2.py
+++ test_lesson2.py
@@ -1,20 +1,27 @@
 import pytest
-from selene.support.shared import browser
+#from selene.support.shared import browser
 from selene import be, have
 
 
 @pytest.fixture
 def open():
      pass
-#     browser.open('https://google.com')
+
+     yield
+
+
+     browser.open('https://google.com')
 
 
 
-def test_search():
+
+
+def test_search(open, cloce):
      pass
 #     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
  #    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
+@pytest.fixture
 def cloce():
      pass
 
